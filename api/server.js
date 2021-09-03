9a8fdd0dfd7408d55441e4562bc1d5163bfae62b
Leu Family <romanianfobjr@gmail.com>
2021-09-03 12:31:28 -0500

    server running on port 5000

--- api/server.js
+++ api/server.js
@@ -5,5 +5,6 @@ const server = express();
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
 
 module.exports = server;
